272d2523c5a15d33e5ad4eefa610872c06e4f54b
reinnatan <reinnatan@gmail.com>
2023-11-01 10:32:53 +0700

    add racks data

--- src/main/java/com/simple/warehouse/controller/RacksServlet.java
+++ src/main/java/com/simple/warehouse/controller/RacksServlet.java
@@ -1,9 +1,19 @@
 package com.simple.warehouse.controller;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.Set;
+
+import com.simple.warehouse.dao.RackDao;
+import com.simple.warehouse.dao.WarehouseDao;
+import com.simple.warehouse.enitty.Rack;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
@@ -12,11 +22,40 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class RacksServlet extends HttpServlet {
+	RackDao dao;
+	
+	@Override
+	public void init() throws ServletException {
+		dao = new RackDao();
+	}
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String idWarehouse = req.getParameter("warehouseId");	
+		String name = req.getParameter("rackName");	
+		//Integer count = Integer.parseInt(req.getParameter("count"));	
+		//String inBoundDate =  req.getParameter("inboundDate");
+		//SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+		//Date dateParse=null;
+		//try {
+		//	dateParse = formatter.parse(inBoundDate);
+		//} catch (java.text.ParseException e) {
+			// TODO Auto-generated catch block
+		//	e.printStackTrace();
+		//}
 		
+		//add data to new Rack
+		Rack newRack = new Rack();
+		newRack.setName(name);
+		//newRack.setCount(count);
+		//newRack.setDateInbound(dateParse);
+		dao.addRacks(newRack, idWarehouse);
+		
+		//forward to view
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/views/racks/listRacks.jsp");
+		dispatcher.forward(req, resp);
 	}
+	
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
--- src/main/webapp/header.jsp
+++ src/main/webapp/header.jsp
@@ -4,14 +4,19 @@
 <html>
 <head>
 <meta charset="UTF-8">
+  <!-- boostrap libs-->
   <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>	
   
+  <!-- datatable libs -->
   <link href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css" rel="stylesheet">
  <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  
+  <!-- sweet alert libs -->
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
